f0600e6c517f6cc1584f0bc5dca5ae6eea726a23
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-15 18:02:18 +0100

    src

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import {useState} from "react";
 
 function App() {
-const [likes, setLikes] = useState(0);
-  return (
-    <>
-        <div>Jestem Groszek, a o to mój licznik lajków! </div>
-        <p>Liczba lajków:{likes}</p>
-        <button onClick={() => setLikes(likes + 1)} >Polub ❤️</button>
-        <button onClick={() => setLikes(likes > 0 ? likes - 1 : 0)}>Odlub 💔</button>
-    </>
-  )
+    const [likes, setLikes] = useState(0);
+    return (
+        <>
+            <div>Jestem Groszek, a o to mój licznik lajków!</div>
+            <p>Liczba lajków:{likes}</p>
+            <button onClick={() => setLikes(likes + 1)}>Polub ❤️</button>
+            <button onClick={() => setLikes(likes > 0 ? likes - 1 : 0)}>Odlub 💔</button>
+        </>
+
+    )
 }
 
-export default App
+export default App;
