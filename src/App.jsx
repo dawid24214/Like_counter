4faccd573fbdf72274d8328e45b7e1fd85acd650
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-15 17:38:07 +0100

    counter_minusLike

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ const [likes, setLikes] = useState(0);
     <>
         <div>Jestem Groszek, a o to mój licznik lajków! </div>
         <p>Liczba lajków:{likes}</p>
-        <button onClick={() => setLikes(likes + 1)} >Polub❤️</button>
+        <button onClick={() => setLikes(likes + 1)} >Polub ❤️</button>
+        <button onClick={() => setLikes(likes > 0 ? likes - 1 : 0)}>Odlub 💔</button>
     </>
   )
 }
