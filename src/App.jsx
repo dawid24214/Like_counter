88d55f3b119d277e6eff9551f0f80c9364ce5747
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-07-08 16:23:40 +0200

    new_vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import {useState} from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    const [likes, setLikes] = useState(0);
-    return (
-        <>
-            <div>Jestem Groszek, a o to mój licznik lajków! </div>
-            <p>Liczba lajków:{likes}</p>
-            <button onClick={() => setLikes(likes + 1)} >Polub ❤️</button>
-            <button onClick={() => setLikes(likes > 0 ? likes - 1 : 0)}>Odlub 💔</button>
-        </>
-    )
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
